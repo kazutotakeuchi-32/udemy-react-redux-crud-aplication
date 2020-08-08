f5a32ac1d45061e928ae3b371e584b307444eb4a
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2020-08-08 20:17:49 +0900

    create class component and functional

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 // function App() {
 //   return (  <div className="App">Hello , world!</div>);
@@ -9,7 +9,8 @@ function name(){
 }
 // class App extends Component{
 //   render(){
-//     return( React.createElement(
+//     return(
+//       React.createElement(
 //       "div",
 //         null,
 //         "Hello,World"
@@ -23,20 +24,35 @@ function clickCall(){
  const greeting = "Hi Tom!"
  const dom = `<h1 className="ff">${greeting}</h1>`
 
-class App extends Component{
-  render(){
-    return(
-      <React.Fragment>
-          <label htmlFor="bar">bar</label>
-          <input type ="text" onKeyUp={()=>{ document.querySelector("body").insertAdjacentHTML("beforeend",document.querySelector("input").value)}} />
-      </React.Fragment>
-    // <div>
-    //   <h1 className="App">Hello , world!</h1>
-    //   dom
-    //   <h2>my name is {name("kazuto")}</h2>
-    // </div>
+// class App extends Component{
+//   render(){
+//     return(
+//       <React.Fragment>
+//         <h1 className="App">Hello , world!</h1>
+//           <label htmlFor="bar">bar</label>
+//           <input type ="text" onKeyUp={()=>{ document.querySelector("body").insertAdjacentHTML("beforeend",document.querySelector("input").value)
 
-    )
-  }
+//         }} />
+//       </React.Fragment>
+//     // <div>
+//     //   <h1 className="App">Hello , world!</h1>
+//     //   dom
+//     //   <h2>my name is {name("kazuto")}</h2>
+//     // </div>
+
+//     )
+//   }
+// }
+const App = ()=>{
+  return (
+  <div>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+  </div>)
 }
+const Cat=()=>{
+  return <div>Meow!</div>
+}
+
 export default App;
