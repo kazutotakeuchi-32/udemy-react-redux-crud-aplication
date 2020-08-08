90f153ffd9514e72545a782b7360ecd201c966b9
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2020-08-08 22:44:58 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { render } from 'react-dom';
 // function App() {
 //   return (  <div className="App">Hello , world!</div>);
@@ -47,7 +48,6 @@ const App = ()=>{
   const profiles =[
     {name:"Taro",age:20},
     {name: "kazuto",age:20},
-    {name: "kazuto"}
 
   ]
   return (
@@ -68,8 +68,73 @@ return(
 </div>
   )
 }
-User.defaultProps ={
-  age: 1
+User.propTypes ={
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
-export default App;
+
+// function formatDate(date) {
+//   return date.toLocaleDateString();
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <UserInfo user={props.author}/>
+//       <div className="Comment-text">{props.text}</div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+// function App(){
+//   const comment = {
+//     date: new Date(),
+//     text: 'I hope you enjoy learning React!',
+//     author: {
+//       name: 'Hello Kitty',
+//       avatarUrl: 'https://placekitten.com/g/64/64',
+//     },
+//   };
+//  return( <Comment
+//       date={comment.date}
+//       text={comment.text}
+//       author={comment.author}
+//     />)
+// }
+
+// function Avatar(props){
+//   return(
+//     <img className ="Avatar"
+//       src={props.user.avatarUrl}
+//       alt ={props.user.name}
+//     />
+//   )
+// }
+
+// function UserInfo(props){
+//   return(
+//     <div className="UserInfo">
+//       <Avatar user={props.user} />
+//       <div className="UserInfo-name">
+//         {props.user.name}
+//       </div>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   <Comment
+//     date={comment.date}
+//     text={comment.text}
+//     author={comment.author}
+//   />,
+  // document.getElementById('root')
+// );
+
+export default  App;
