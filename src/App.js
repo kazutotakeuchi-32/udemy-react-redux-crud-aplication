279c35cf59ab801a4dde3edd25b3d51a375ed35a
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2020-08-08 16:45:44 +0900

    learn] jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+// function App() {
+//   return (  <div className="App">Hello , world!</div>);
+// }
 
-function App() {
-  return (  <div className="App"><h1>Hello , world!</h1></div>);
+function name(){
+  return name
 }
+// class App extends Component{
+//   render(){
+//     return( React.createElement(
+//       "div",
+//         null,
+//         "Hello,World"
+//       )
+//     );
+//   }
+// }
+function clickCall(){
+  alert(dom)
+}
+ const greeting = "Hi Tom!"
+ const dom = `<h1 className="ff">${greeting}</h1>`
 
+class App extends Component{
+  render(){
+    return(
+      <React.Fragment>
+          <label htmlFor="bar">bar</label>
+          <input type ="text" onKeyUp={()=>{ document.querySelector("body").insertAdjacentHTML("beforeend",document.querySelector("input").value)}} />
+      </React.Fragment>
+    // <div>
+    //   <h1 className="App">Hello , world!</h1>
+    //   dom
+    //   <h2>my name is {name("kazuto")}</h2>
+    // </div>
+
+    )
+  }
+}
 export default App;
